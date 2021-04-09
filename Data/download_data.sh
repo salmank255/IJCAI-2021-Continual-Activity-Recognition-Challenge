b4410faecefe049b91ece9783d3471c7ae407aaf
Salman Khan <32925691+salmank255@users.noreply.github.com>
2021-04-09 10:38:18 +0100

    Create download_data.sh

---
+++ Data/download_data.sh
@@ -0,0 +1,20 @@
+
+#Downloading contiguous videos in zip
+wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=1Qn_QlDOPAAYj7CrehU8aVnng2CpvFlg5' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=1Qn_QlDOPAAYj7CrehU8aVnng2CpvFlg5" -O contiguous_videos.zip
+#Unzipping the videos
+unzip contiguous_videos.zip
+#Removing the zip file
+rm contiguous_videos.zip
+#Downloading short gap videos in zip
+wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=1Rc2T_DUrzzJ7K2e5f_nH21_ZeVzwPaxa' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=1Rc2T_DUrzzJ7K2e5f_nH21_ZeVzwPaxa" -O short_gap.zip
+#Unzipping the videos
+unzip short_gap.zip
+#Removing the zip file
+rm short_gap.zip
+#Downloading long gap videos in zip
+wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=1ZpU3YuZwwcZH033dxbqZm5jZLqczJdiu' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=1ZpU3YuZwwcZH033dxbqZm5jZLqczJdiu" -O long_gap.zip
+#Unzipping the videos
+unzip long_gap.zip
+#Removing the zip file
+rm long_gap.zip
+#
