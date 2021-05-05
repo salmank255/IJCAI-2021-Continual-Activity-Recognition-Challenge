bcc82029cad5d712313d27fa98a0b7722140055a
Salman Khan <32925691+salmank255@users.noreply.github.com>
2021-05-05 14:05:04 +0100

    Update download_data.sh

--- Data/download_data.sh
+++ Data/download_data.sh
@@ -18,4 +18,4 @@ unzip long_gap.zip
 #Removing the zip file
 rm long_gap.zip
 #
-wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=1OGDUypVKR_lqwTwIXNUkVU0VR5z4fymL' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=1OGDUypVKR_lqwTwIXNUkVU0VR5z4fymL" -O Annotation.zip
+wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=1OGDUypVKR_lqwTwIXNUkVU0VR5z4fymL' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=1OGDUypVKR_lqwTwIXNUkVU0VR5z4fymL" -O Annotation.json
